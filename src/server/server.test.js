a8d4e1271631221ffef478b472ec175178b664a5
Ash Connolly <ashconnollydigital@gmail.com>
2026-07-09 16:06:21 +0100

    test fix

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,7 +1,12 @@
 import { initialiseServer } from '../test-utils/initialise-server.js'
 import { statusCodes } from './common/constants/status-codes.js'
+import { config } from '../config/config.js'
 
 describe('createServer', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   test('registers extra plugins passed via mockedPlugins', async () => {
     const fakePlugin = {
       plugin: {
@@ -27,6 +32,20 @@ describe('createServer', () => {
 
     expect(statusCode).toBe(statusCodes.ok)
     expect(result).toEqual({ ok: true })
+
+    await server.stop({ timeout: 0 })
+  })
+
+  test('sets server uri to appBaseUrl in development', async () => {
+    const actualGet = config.get.bind(config)
+    const expectedUri = actualGet('appBaseUrl')
+    vi.spyOn(config, 'get').mockImplementation((key) =>
+      key === 'isDevelopment' ? true : actualGet(key)
+    )
+
+    const server = await initialiseServer()
+
+    expect(server.info.uri).toBe(expectedUri)
 
     await server.stop({ timeout: 0 })
   })
